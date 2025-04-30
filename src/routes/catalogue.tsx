278af2807fc4281dd2af3496c6d131fa63a062c5
Vasyl Pavliuchenko <vasyl.pavlyuchenko@gmail.com>
2025-04-30 12:39:13 +0000

    WIP: add search to catalogue

--- src/routes/catalogue.tsx
+++ src/routes/catalogue.tsx
@@ -8,11 +8,27 @@ import {
   CardContent,
   CardFooter,
 } from "@/components/ui/card";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
 import { Badge } from "@/components/ui/badge";
 import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Search } from "lucide-react";
+import { Search, Filter } from "lucide-react";
 
 export const Route = createFileRoute("/catalogue")({
   component: Catalogue,
@@ -28,6 +44,24 @@ interface BookDTO {
 function Catalogue() {
   const [books, setBooks] = useState<BookDTO[]>([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [availability, setAvailability] = useState("");
+
+  const filteredBooks = books.filter((book) => {
+    const matchesSearch =
+      book.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      book.authorNames
+        .toString()
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase()) ||
+      book.publicationYear
+        .toString()
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase()) ||
+      book.isbn.includes(searchTerm);
+
+    // make matches availability
+    return matchesSearch;
+  });
 
   useEffect(() => {
     const fetchBooks = async () => {
@@ -52,20 +86,53 @@ function Catalogue() {
             Обери книгу, яку хочеш орендувати
           </p>
         </div>
-        <div className="relative flex w-80 mt-4">
-          <Search className="absolute left-2.5 top-2.5 w-4 h-4 text-muted-foreground"/>
-          <Input
-            type="search"
-            placeholder="Пошук книг за назвою, автором, ISBN"
-            value={searchTerm}
-            className="pl-8"
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
+        <div className="flex items-start gap-1">
+          <div className="relative flex w-80 mt-4">
+            <Search className="absolute left-2.5 top-2.5 w-4 h-4 text-muted-foreground" />
+            <Input
+              type="search"
+              placeholder="Пошук книг за назвою, автором, ISBN"
+              value={searchTerm}
+              className="pl-8"
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon" className="mt-4">
+                <Filter className="h-4 w-4" />
+              </Button>
+            </SheetTrigger>
+            <SheetContent className="flex items-start gap-4 py-4">
+              <SheetHeader>
+                <SheetTitle>Фільтри</SheetTitle>
+                <SheetDescription>
+                  Налаштуйте параметри пошуку книг
+                </SheetDescription>
+              </SheetHeader>
+              <div className="px-4 space-y-2">
+                <Label htmlFor="availability">Наявність</Label>
+                <Select value={availability} onValueChange={setAvailability}>
+                  <SelectTrigger id="availability">
+                    <SelectValue placeholder="Усі книги" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="all">Усі книги</SelectItem>
+                    <SelectItem value="available">Є в наявності</SelectItem>
+                    <SelectItem value="unavailable">
+                      Немає в наявності
+                    </SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </SheetContent>
+          </Sheet>
         </div>
       </div>
 
       <div className="grid grid-cols-3 items-stretch gap-5">
-        {books.map((book) => (
+        {filteredBooks.map((book) => (
           <Card className="grid grid-rows-[1fr_auto]">
             <CardHeader>
               <CardTitle className="flex justify-between items-start">
